ce13591848aa47ad78e6dfdfb588d6cb00e4ce7f
Brij Malhotra <brij.g.malhotra@gmail.com>
2017-03-20 16:13:49 +0530

    Created a training data

---
+++ trigram_from_corpus.py
@@ -0,0 +1,22 @@
+import nltk
+from nltk.util import ngrams
+from nltk.corpus import brown #Load the brown corpus
+
+#Run download for the first time to download the nltk stuff
+#nltk.download()
+
+ngram = 3 #A trigram model for trigram training data
+sentences = brown.sents(categories = 'adventure')
+words = brown.words(categories = 'adventure') #This can be any category
+
+# print len(sentences),len(words)
+
+#Store the model to a text file
+text_file = open('trigram_adventure.txt','w')
+for i,word in enumerate(words):
+	if i > len(words)-ngram:
+		break	 
+	value = words[i] + '\t' + words[i+1] + '\t' + words[i+2] + '\n'
+	text_file.write(value)
+
+text_file.close()
