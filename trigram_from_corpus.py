1af491cf41bb8dc3b5f524151f125fe98e5527cc
Brij Malhotra <brij.g.malhotra@gmail.com>
2017-03-20 16:35:36 +0530

    removed puctuations from the training data

--- trigram_from_corpus.py
+++ trigram_from_corpus.py
@@ -12,10 +12,15 @@ words = brown.words(categories = 'adventure') #This can be any category
 # print len(sentences),len(words)
 
 #Store the model to a text file
+punctuations = ['.',',','?','\'','\"',"!",":",";",'/']
 text_file = open('trigram_adventure.txt','w')
 for i,word in enumerate(words):
 	if i > len(words)-ngram:
-		break	 
+		break
+
+	#continue if the word is punctuation	 
+	if word in punctuations or words[i+1] in punctuations or words[i+2] in punctuations:
+		continue
 	value = words[i] + '\t' + words[i+1] + '\t' + words[i+2] + '\n'
 	text_file.write(value)
 
