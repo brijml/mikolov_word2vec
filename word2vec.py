b504480da7adc6800deae616d3ab1fec7af179f9
Brij Malhotra <brij.g.malhotra@gmail.com>
2017-04-29 16:57:29 +0530

    Documentation

--- word2vec.py
+++ word2vec.py
@@ -7,21 +7,40 @@ from sklearn.externals import joblib
 
 def init_vectors(dim):
 	
-	#Initialising the matrices using a random samples of a normal distribution
+	"""Initialising the matrices using a random samples of a normal distribution
+	
+	args
+	-----
+	Parameter : int;the length of the dimensiions for each word vector
+	
+	Returns : Numpy Matrix; Initialised words vectors for each word in the vocabulary	
+	"""
 	C = 0.01 * np.random.randn(len_vocab,dim)/np.sqrt(len_vocab*dim)
 	return np.matrix(C)
 
 def one_hot(word):
 
-	#One hot representation of the word where the index for the word is a one and other indices are zeros
+	"""One hot representation of the word where the index for the word is a one and other indices are zeros
+	
+	args
+	----
+	Parameter : string, The word to match in the Vocabulary to be represented as a one-hot vector
+	
+	Returns : Numpy array,index; The one hot vector and the index of the word in the vocabulary
+	"""
+	  
 	ret = np.zeros(len_vocab,dtype = np.uint8)
 	index = vocabulary.index(word)
 	ret[index] = 1
 	return ret,index
 
 def negative_distribution():
-	#Sample negative words from the vocabulary
-	#Probability is a function of count(word)/|Vocabulary| to power of 3/4
+	"""
+	Sample negative words from the vocabulary
+	Probability is a function of count(word)/|Vocabulary| to power of 3/4
+	
+	Returns a sample according to the Probabibilty distribution 
+	"""
 	index = np.random.randint(low=0, high=table_size, size=1)
 
 	return table[index][0]
@@ -37,6 +56,7 @@ def sigmoid(z,func = "logistic"):
 def one_training_example(x1,x2,U,V):
 
 	#Gradient Descent on both the matrices for the input word and the outside words for a single training example
+	#A recursive function to iteratively update the word matrices
 	k = 20
 	learning_rate = 0.01
 	vc,vw = x1[0]*U,x2[0]*V
@@ -96,7 +116,7 @@ def training(words,window = 2,hidden_units = 300,epoch = 1):
 	Returns : Numpy Matrix
 			Learned vector representation of the words in the corpus
 	
-	The objective function is to maximise the probabitlity of the words in the co-occuring the window
+	The objective is to maximise the probabitlity of the words in the co-occuring the window
 	"""
 
 	U = init_vectors(hidden_units)
@@ -138,20 +158,28 @@ if __name__ == '__main__':
 	#Regular expression for all the Punctuations
 	REGEX = "[^a-zA-Z0-9]"
 	
-	categories = brown.categories() #List of all the catgories in the brown corpus
+	#List of all the catgories in the brown corpus and store all the words from the corus in a list
+	categories = brown.categories()
 	words = []
 	for category in categories:
-		words_temp = brown.words(categories = 'adventure') #words taken from a particular category of the brown corpus
+		words_temp = brown.words(categories = category) #words taken from a particular category of the brown corpus
 		words.extend(words_temp)
 
+	#Counter object to calculate the frequency of each word in the corpus
 	count = Counter(words)
-	vocabulary = count.keys()
+	vocabulary = count.keys() #List of all the words in the vocabulary
 	len_vocab = len(vocabulary)
 	
+	#Frequency of each word in the corpus to form a distribution for the negative sampling 
 	freq = np.float32(np.array(count.values()))
-	numerator_distribution = np.power(freq,0.75)
-	norm_term = sum(numerator_distribution)
+	numerator_distribution = np.power(freq,0.75) #Frequency raised to the power 3/4 which is empirical
+	norm_term = sum(numerator_distribution)  	#Normalisisng factor
 	distribution = numerator_distribution/norm_term
+	
+	#The way to form to select a word is from a table of word indices and store word indices multiple times according to the word
+	#frequency given by formula freq(word)/len(vocabulary) times the table_size and sample a random integer from the (0,len(table)
+	#and take the index at location of sample which is the index of the sampled word.
+	#Refer the negative_sampling() method.
 	table_size = int(1e8)
 	table = np.zeros(table_size,dtype = np.int16)
 	previous_j = 0
@@ -161,9 +189,9 @@ if __name__ == '__main__':
 		previous_j = previous_j+j
 
 	word_representations = training(words)
-
+	
+	#Word dictionary where key is the word and value is the word vector.
 	word_dict = {}
-
 	for i,word in enumerate(vocabulary):
 		word_dict[word] = word_representations[i]
 
